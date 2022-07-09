d03d6dcbb18379364b391adb78af7bf82ddefd19
tarikdn <tariktrkmnsecond@outlook.com>
2022-07-09 13:31:42 +0200

    added first version of the calc

--- main.py
+++ main.py
@@ -8,4 +8,28 @@ def number_choice():
         print("The answer is: ")
         print(sum)
 
+    if som_type == 'aftrekken':
+        x = input("Type a number")
+        y = input("Type a number")
+        sub= int(x)-int(y)
+
+        print("The answer is: ")
+        print(sub)
+
+    if som_type == 'keren':
+        x = input("Type a number")
+        y = input("Type a number")
+        mul= int(x)*int(y)
+
+        print("The answer is: ")
+        print(mul)
+
+    if som_type == 'delen':
+        x = input("Type a number")
+        y = input("Type a number")
+        dev= int(x)/int(y)
+
+        print("The answer is: ")
+        print(dev)
+
 number_choice()
